4f1be4fc6d8b5e316d532d78bdbd3dc87969506f
Marc Yannick <yannickcoulibaly220@gmail.com>
2025-03-21 08:41:20 +0100

    news api service

--- news-central-backend/src/routes/news.js
+++ news-central-backend/src/routes/news.js
@@ -5,45 +5,14 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
     try {
-        const {popularity, keyword, category, startDate, endDate, string, source, url } = req.query;
+        const {popularity, keyword, category, startDate, endDate, searchString, source, url } = req.query;
 
+        console.log(req.query)
         const [newsAPI] = await Promise.all([
-            fetchNewsFromNewsAPI(keyword, category, startDate, endDate),
+            fetchNewsFromNewsAPI(keyword, category, startDate, endDate, searchString, source, url),
         ]);
 
         let allArticles = [...newsAPI];
-
-        // Appliquer les filtres supplémentaires côté backend
-        // if (source) {
-        //     allArticles = allArticles.filter(article =>
-        //         article.source.toLowerCase().includes(source.toLowerCase())
-        //     );
-        // }
-        //
-        // if (query) {
-        //     allArticles = allArticles.filter(article =>
-        //         article.title.toLowerCase().includes(query.toLowerCase()) ||
-        //         (article.description && article.description.toLowerCase().includes(query.toLowerCase()))
-        //     );
-        // }
-        //
-        // if (startDate) {
-        //     allArticles = allArticles.filter(article =>
-        //         new Date(article.publishedAt) >= new Date(startDate)
-        //     );
-        // }
-        //
-        // if (endDate) {
-        //     allArticles = allArticles.filter(article =>
-        //         new Date(article.publishedAt) <= new Date(endDate)
-        //     );
-        // }
-        //
-        // if (popularity === "desc") {
-        //     allArticles.sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
-        // } else if (popularity === "asc") {
-        //     allArticles.sort((a, b) => new Date(a.publishedAt) - new Date(b.publishedAt));
-        // }
 
         res.json({articles: allArticles});
     } catch (error) {
--- news-central-backend/src/services/newsapiService.js
+++ news-central-backend/src/services/newsapiService.js
@@ -1,27 +1,51 @@
 const axios = require("axios");
 const dayjs = require('dayjs');
 
-const fetchNewsFromNewsAPI = async (keyword = "latest", category, startDate, endDate, string, source, url) => {
+const fetchNewsFromNewsAPI = async (
+    keyword = "latest",
+    category,
+    startDate,
+    endDate,
+    source,
+    url,
+    sortBy = "popularity",
+    searchString
+) => {
     try {
-        let url = `https://newsapi.org/v2/everything?q=${keyword}&apiKey=${process.env.NEWS_API_KEY}`;
+        let apiUrl = `https://newsapi.org/v2/everything?q=${keyword}&sortBy=${sortBy}&apiKey=${process.env.NEWS_API_KEY}`;
 
         if (category) {
-            url = `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=${process.env.NEWS_API_KEY}`;
+            apiUrl = `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=${process.env.NEWS_API_KEY}`;
         }
 
-        if (startDate) url += `&from=${dayjs(startDate).format('YYYYMMDD')}`;
-        if (endDate) url += `&to=${dayjs(startDate).format('YYYYMMDD')}`;
+        if (searchString) apiUrl += `&q=${encodeURIComponent(searchString)}`;
 
-        const response = await axios.get(url);
+        if (source) apiUrl += `&sources=${encodeURIComponent(source)}`;
 
-        return response.data.articles.map(article => ({
-            articleTitle: article.title,
-            articleDescription: article.description,
+        if (startDate) apiUrl += `&from=${dayjs(startDate).format('YYYY-MM-DD')}`;
+        if (endDate) apiUrl += `&to=${dayjs(endDate).format('YYYY-MM-DD')}`;
+
+        console.log(apiUrl);
+        const response = await axios.get(apiUrl);
+
+        let articles = response.data.articles.map(article => ({
+            title: article.title,
+            description: article.description,
             imageUrl: article.urlToImage,
             url: article.url,
             source: article.source.name,
-            publishedAt: article.publishedAt
+            publishedAt: article.publishedAt,
+            content: article.content
         }));
+
+        if (url) {
+            articles = articles.filter(article =>
+                article.url.toLowerCase().includes(url.toLowerCase())
+            );
+        }
+
+        return articles;
+
     } catch (error) {
         console.error("Erreur NewsAPI:", error);
         return [];
